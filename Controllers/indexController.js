470ca1509de646c6e16135290473f2dfeb7ce23d
Adarsh <adarshkmeethal@gmail.com>
2022-02-22 23:06:19 +0530

    Header social media logo

---
+++ Controllers/indexController.js
@@ -0,0 +1,5 @@
+
+exports.home = (req,res,next)=>{
+    res.render('index');
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const indexController = require('../Controllers/indexController');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
+router.route('/').get(indexController.home)
 module.exports = router;
