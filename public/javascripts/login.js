0cad9352f5cf2eadbc5bd5671796b52b3e512485
Adarsh <adarshkmeethal@gmail.com>
2022-03-15 13:19:59 +0530

    Error solved and passed cookies to the browser

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,20 +1,31 @@
 console.log('login')
 
-const login = (email,password)=>{
-    alert(email,password);
+const login = async (email,password)=>{
+    console.log(email,password);
+    try{
+        const result =  await axios({
+              method:'POST',
+              url:'http://localhost:3000/api/v1/users/login',
+              data:{
+                  email:email,
+                  password:password,
+       
+              }
+          });
+          console.log(result);
+    } catch(errr){
+        console.log(err.response.data);
+    }
 }
 
 
-// document.getElementById('form').addEventListener('submit', e => {
-//     e.preventDefault();
-//     console.log('clicked');
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-//     console.log(email)
-//     console.log(password);
-//     login(email, password);
-// })    
-
-document.getElementById('sbt').addEventListener('click',function(){
+document.querySelector('.form').addEventListener('submit', e => {
+    e.preventDefault();
     console.log('clicked');
-})
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log(email)
+    console.log(password);
+    login(email, password);
+})    
+
