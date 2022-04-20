19c7d885395c55cfe1dfb2847f8fe967a112296c
Adarsh <adarshkmeethal@gmail.com>
2022-04-20 23:04:34 +0530

    production

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+
     console.log('App running on port 3000...');
 })
 
