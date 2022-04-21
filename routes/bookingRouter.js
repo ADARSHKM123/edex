76fe8f5197165f17697d65cc630ed8e5caae15d8
ADARSH <67700780+ADARSHKM123@users.noreply.github.com>
2022-04-21 15:29:19 +0530

    Update bookingRouter.js

--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const bookingController = require('../Controllers/bookingController');
-<<<<<<< HEAD
 const authController = require('../Controllers/authController');
-=======
-const authController = require('../Controllers/authController');
->>>>>>> 98af75fe089ae1c2a36d8359c301a83c292ca066
 
 const router = express.Router();
 
